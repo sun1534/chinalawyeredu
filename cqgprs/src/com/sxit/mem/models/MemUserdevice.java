bf74419d51adb9f232f91728b9d0de8bfef8785f
hfeng.liu <hfeng.liu@gmail.com>
2010-11-13 08:15:03 +0000

    mem的models

---
+++ cqgprs/src/com/sxit/mem/models/MemUserdevice.java
@@ -0,0 +1,36 @@
+package com.sxit.mem.models;
+
+/**
+ * MemUserdevice entity.
+ * 
+ * @author MyEclipse Persistence Tools
+ */
+
+public class MemUserdevice implements java.io.Serializable {
+
+	// Fields
+
+	private MemUserdeviceId id;
+
+	// Constructors
+
+	/** default constructor */
+	public MemUserdevice() {
+	}
+
+	/** full constructor */
+	public MemUserdevice(MemUserdeviceId id) {
+		this.id = id;
+	}
+
+	// Property accessors
+
+	public MemUserdeviceId getId() {
+		return this.id;
+	}
+
+	public void setId(MemUserdeviceId id) {
+		this.id = id;
+	}
+
+}
